753f9a251d65e24bbd87021c87edc7a5c5eac944
Josef Benninghoff <jsbenning4@gmail.com>
2018-05-20 18:07:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const driversByName = function(drivers){
 }
 
 const totalRevenue = function(drivers){
-  let sorted = drivers.reduce(function(el, sum){
+  let sorted = drivers.reduce(function(sum, el){
     return el.revenue + sum;
   }, 0);
   return sorted;
